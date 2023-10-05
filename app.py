50dc0ac0952b3aef0efd9cc843033c0edd371fa7
Support <support@mindblownlab.studio>
2023-10-04 21:23:08 -0300

    INSTALL

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-import sys
 import os
+import sys
 
-root_path = os.path.join(os.path.dirname(__file__))
+root_path = os.path.join(sys.argv[1])
 libraries_path = os.path.join(root_path, "library")
+
+# os.environ["PYTHONPATH"] = os.path.join(root_path, "Python")
 
 if root_path not in sys.path:
     sys.path.append(root_path)
 
 if libraries_path not in sys.path:
     sys.path.append(libraries_path)
+
+print(os.environ["PYTHONPATH"])
+print(root_path)
+print(libraries_path)
 
 from PyQt5 import QtWidgets
 from importlib import reload
--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 set scriptpath=%~dp0
-set PYTHONPATH=%scriptpath%Python
-python app.py
+set PYTHONPATH=%scriptpath%Python;%scriptpath%library
+python app.py %scriptpath%
 exit /b 0
 pause
